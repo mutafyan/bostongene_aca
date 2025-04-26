8c787cc106eb1bac7519a0acc54666c62efc15b7
Davit Mutafyan <davitmutafyan@gmail.com>
2025-04-26 22:44:00 +0400

    add localstorage sync

--- react/fake-store/src/context/CartContext.jsx
+++ react/fake-store/src/context/CartContext.jsx
@@ -1,10 +1,17 @@
-import { createContext, useReducer, useContext } from "react";
+import { createContext, useReducer, useContext, useEffect } from "react";
 import { cartReducer, initialState } from "./cartReducer";
 
 const CartContext = createContext();
 
 export const CartProvider = ({ children }) => {
-  const [state, dispatch] = useReducer(cartReducer, initialState);
+  const savedCart = JSON.parse(localStorage.getItem("cart")) || initialState;
+
+  const [state, dispatch] = useReducer(cartReducer, savedCart);
+
+  useEffect(() => {
+    localStorage.setItem("cart", JSON.stringify(state));
+  }, [state]);
+
   return (
     <CartContext.Provider value={{ cart: state, dispatch }}>
       {children}
